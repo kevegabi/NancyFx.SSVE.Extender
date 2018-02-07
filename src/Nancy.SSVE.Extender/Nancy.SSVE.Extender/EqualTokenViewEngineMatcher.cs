e736df717c5e247907a46d82ea64d874e26022e2
Keve Gábor <kevegabi@gmail.com>
2018-02-07 14:05:43 +0100

    rename some files, and folders

--- src/Nancy.SSVE.Extender/Nancy.SSVE.Extender/EqualTokenViewEngineMatcher.cs
+++ src/Nancy.SSVE.Extender/Nancy.SSVE.Extender/EqualTokenViewEngineMatcher.cs
@@ -2,7 +2,7 @@
 using System;
 using System.Text.RegularExpressions;
 
-namespace NancyFx.SSVE.Extender
+namespace Nancy.SSVE.Extender
 {
     public sealed class EqualTokenViewEngineMatcher : ISuperSimpleViewEngineMatcher
     {
